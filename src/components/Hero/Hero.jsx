f413cd20335aacd3d39e7cdfc1cb68cc52ce8e3f
jubjibb <jibjidapa36854@gmail.com>
2025-05-31 23:55:29 +0700

    fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,12 +7,17 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm Jidapa</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+          I'm a I graduated from the Faculty of Informatics, Burapha University, with a GPA of 3.06.
+
+During my studies, I had the opportunity to develop programming skills and create applications using various languages. I worked on projects such as a movie theater website, a coffee shop website, and an exam checking system, which helped me enhance my problem-solving abilities and effectively collaborate within a team.
+
+In addition, I gained practical experience through an internship at IRPC, which gave me a better understanding of real-world work environments and motivated me to continuously learn new things.
+
+I am committed to continuously developing my skills in this field and am eager to take on new challenges to grow in the career I love.
         </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+        <a href="mailto:jibjidapa36854@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
