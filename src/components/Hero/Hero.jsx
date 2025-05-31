273de43ff2913c16bbb6428e8ecff74f38af052e
jubjibb <jibjidapa36854@gmail.com>
2025-05-31 23:57:09 +0700

    fix 2

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,11 +11,6 @@ export const Hero = () => {
         <p className={styles.description}>
           I'm a I graduated from the Faculty of Informatics, Burapha University, with a GPA of 3.06.
 
-During my studies, I had the opportunity to develop programming skills and create applications using various languages. I worked on projects such as a movie theater website, a coffee shop website, and an exam checking system, which helped me enhance my problem-solving abilities and effectively collaborate within a team.
-
-In addition, I gained practical experience through an internship at IRPC, which gave me a better understanding of real-world work environments and motivated me to continuously learn new things.
-
-I am committed to continuously developing my skills in this field and am eager to take on new challenges to grow in the career I love.
         </p>
         <a href="mailto:jibjidapa36854@gmail.com" className={styles.contactBtn}>
           Contact Me
